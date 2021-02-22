3743d1143a3c6b1bf58af7af81e24426ebd28fa9
Yogesh Singh <yogesh.singh.2810@gmail.com>
2021-02-22 14:32:03 +0530

    initial setup

---
+++ Shared/Test_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_iOSApp.swift
+//  Shared
+//
+//  Created by Yogesh Singh on 22/02/21.
+//
+
+import SwiftUI
+
+@main
+struct Test_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
